7b755efceed9693a7ca887886d8b925c673ac5d6
romil rõbtšenkov <romilrobtsenkov@gmail.com>
2016-04-13 09:20:04 +0300

    added writing to txt file

--- getfeed.php
+++ getfeed.php
@@ -4,6 +4,8 @@
 	require_once("TwitterAPIExchange.php");
 	require_once("config.php");
 	
+	$file_name = "cache.txt";
+	
 	$url = "https://api.twitter.com/1.1/search/tweets.json";
 	$getField = "?q=%23Paris&result_type=recent";
 	$requestMethod = "GET";
@@ -13,7 +15,24 @@
 	$dataFromAPI = $twitter->setGetfield($getField)
 						   ->buildOauth($url, $requestMethod)
 						   ->performRequest();
-	
-	echo $dataFromAPI;
 
+	
+	
+	//var_dump( json_decode($dataFromAPI)->statuses);
+	
+	$object = new StdClass();
+	
+	//millal tegime päringu
+	$object->date = date("c");
+	
+	//saadud tweedid
+	$object->statuses = json_decode($dataFromAPI)->statuses;
+	
+	file_put_contents($file_name, json_encode($object));
+	
+	echo json_encode($object);
+	
+	
+	
+	
 ?> 
